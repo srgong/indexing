6efc3818136698909f1580a012012a4e61c4b6c2
srgong <g.sharonr@gmail.com>
2019-03-18 11:57:06 -0400

    add considerations

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 # Table of Contents
 1. [Introduction](README.md#introduction)
 2. [Approach](README.md#approach)
-3. [To Run](README.md#torun)
+3. [Considerations](README.md#considerations)
+4. [To Run](README.md#torun)
 
 
 # Introduction
@@ -23,6 +24,18 @@ Given text files, the objective is to create a lookup table of words mapped to f
 6) Next we remove duplicate key value pairs.
 7) Lastly, we create our index by grouping by wordIds: `<wordId, [docId1, docId2 ...]`
 
+# Considerations
+
+Processing data in RDDs has a reputation of taking more processing time - especially because data gets [shuffled into the Driver first](https://dzone.com/articles/apache-spark-3-reasons-why-you-should-not-use-rdds) before transformations can be done. I wanted to explore an alternate method of performing this indexing operation using DataFrames to test for potential performance increase. Previously, I believed general performance increase is not fully dependent on the data structure (RDDs vs DataFrames), but the operations done on the data structure. I believe that column operations using DataFrames were most optimal whereas RDDs were best for row level manipulations. 
+
+`//using data frames to set up wordId, docId`
+
+`val df = sqlContext.read.text(input).toDF()
+.select(explode(split(lower(col("value")), "[^\\w]") as "wordId"),regexp_extract(input_file_name,"([0-9]*)$",1) as "docId")
+.distinct
+.as[(String, String)].rdd.groupByKey()`
+
+In my alternate method, the main difference is how I chose to break out the column of arrays using DataFrames `explode` instead of RDDs `flatMap`. In testing for performance increase, `explode` took longer (I imagine by orders of magnitude depending on data size). While DataFrame operations may have a better performance when calculating aggregations (ie count, sum, max), row level operations are still done better in RDDs (flatMap, keyBy).  Note that I am using Spark 2.3.1 so this [explode optimization](https://issues.apache.org/jira/browse/SPARK-21657) does not apply.   
 
 # To Run
 1. `sbt assembly`
--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,5 +1,6 @@
-import org.apache.spark.{SparkConf}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
 /**
@@ -12,39 +13,41 @@ object Driver {
     .set("spark.input.file", "src/main/resources/")
     .set("spark.output.file", "src/main/output/")
 
+  /*
+   * Param: DataFrame
+   * Output: RDD of <wordId, docId>
+   *
+   * DataFrames are processed by following this logic:
+   * Extract only the last numbers from the file path
+   * Normal words
+   * Create key value pair
+   * Break each line into its own word
+   * Set the key to the word
+   * Key by docId
+   * Rearrange tuple to just (docId, wordId)
+   * Map docId to wordId
+   * Remove duplicate key value pairs
+   * Create arrays where there are similar keys
+   */
+  def createIndex(df: DataFrame): RDD[(String, Iterable[String])] = {
+    df.select(
+      regexp_extract(input_file_name,"([0-9]*)$",1) as "docId",
+      lower(col("value")) as "wordId")
+      .as[(String, String)].rdd
+      .flatMapValues(line => line.split("[^\\w]"))
+      .keyBy(x => x._2)
+      .mapValues(x => x._1)
+      .distinct
+      .groupByKey()
+  }
+
   def main(args: Array[String]): Unit = {
     val spark: SparkSession =
       SparkSession.builder().config(sparkConf).getOrCreate()
     val sqlContext = spark.sqlContext
 
     val input = sparkConf.get("spark.input.file")
-
-    import spark.implicits._
-     val df = sqlContext.read.text(input).toDF()
-          .select(
-//             extract only the last numbers from file path
-            regexp_extract(input_file_name,"([0-9]*)$",1) as "docId",
-//             normalize words
-            lower(col("value")) as "wordId")
-//           create key value pair
-          .as[(String, String)].rdd
-          // break each line into its own word
-          .flatMapValues(line => line.split("[^\\w]"))
-          // set the key to the word
-          .keyBy(x => x._2) // key by docId
-          // rearrange tuple to just (docId, wordId)
-          .mapValues(x => x._1) // map docId -> wordId
-          // remove duplicate key value pairs
-          .distinct
-          // create arrays where there are similar keys
-          .groupByKey()
-
-    // using data frames to set up wordId, docId
-    //    val df = sqlContext.read.text(input).toDF()
-    //      .select(explode(split(lower(col("value")), "[^\\w]") as "wordId"),regexp_extract(input_file_name,"([0-9]*)$",1) as "docId")
-    //      .distinct
-    //      .as[(String, String)].rdd.groupByKey()
-
+    val df = createIndex(sqlContext.read.text(input).toDF())
 
     df.repartition(255).saveAsTextFile(sparkConf.get("spark.output.file"))
   }
